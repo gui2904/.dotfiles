f93d5fa843f30815dbc662e81e3e89e77e555b10
gui2904 <gfulco4@gmail.com>
2025-01-23 11:10:28 -0500

    test

--- modules/programs/editors/emacs.nix
+++ modules/programs/editors/emacs.nix
@@ -1,32 +1,27 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: let
+{ lib, config, pkgs, ... }: let
   emacsDir = "${config.home.homeDirectory}/.config/emacs";
 in {
   options.clover.programs.emacs = {
     enable = lib.mkEnableOption "Enable Emacs";
-    client.enable = lib.mkEnableOption "emacs client";
-    standalone.enable = lib.mkEnableOption "emacs standalone";
+    client.enable = lib.mkEnableOption "Enable Emacs Client";
+    standalone.enable = lib.mkEnableOption "Enable Emacs Standalone";
   };
 
   config = lib.mkIf config.clover.programs.emacs.enable {
     programs.emacs = {
       enable = true;
       package = pkgs.emacs;  # Use the default Emacs package
-  
 
       extraConfig = ''
         (setq user-emacs-directory (expand-file-name "~/.config/emacs/"))
         (load-file (concat user-emacs-directory "init.el"))
       '';
     };
-    
+
+    # Assuming you want to enable Emacs as a service (optional)
     services.emacs = {
       enable = true;
       package = pkgs.emacs;
     };
-  }; 
- }
+  };
+}
