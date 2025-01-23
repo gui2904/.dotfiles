9ce8a1281635a36747a7a9e5bfa188370e08a381
gui2904 <gfulco4@gmail.com>
2025-01-23 10:19:23 -0500

    test

--- hosts/clover/configuration.nix
+++ hosts/clover/configuration.nix
@@ -6,6 +6,10 @@
       ./hardware-configuration.nix
     ];
 
+  clover.users = {
+    laptop.enable = true;
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
