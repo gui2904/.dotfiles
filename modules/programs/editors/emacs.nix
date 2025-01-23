f696fb084b865c7f90b267de44a7dcc14e8b367c
gui2904 <gfulco4@gmail.com>
2025-01-23 11:33:53 -0500

    test

--- modules/programs/editors/emacs.nix
+++ modules/programs/editors/emacs.nix
@@ -4,7 +4,7 @@
 in {
   options.clover.programs.emacs = {
     enable = lib.mkEnableOption "Enable Emacs";
-    client.enable = lib.mkEnableOption "Enable Emacs Client";
+    client.enable = lib.mkEnableOption "emacs client";
     standalone.enable = lib.mkEnableOption "Enable Emacs Standalone";
   };
 
