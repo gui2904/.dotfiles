ec7e80574c56d721da5b0c204956140b8ca418ab
Gui <gfulco4@gmail.com>
2025-01-24 01:22:10 -0500

    tst

--- modules/services/hyprland.nix
+++ modules/services/hyprland.nix
@@ -2,7 +2,6 @@
 
 {
   systemd.user.services.hyprland = {
-    description = "Hyprland as a service";
     serviceConfig.ExecStart = "${pkgs.hyprland}/bin/hyprland";
     wantedBy = [ "default.target" ];
   };
