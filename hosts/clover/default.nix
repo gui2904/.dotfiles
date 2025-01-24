821ba6e1aa78710d5fed964772ea41c332a8969a
Gui <gfulco4@gmail.com>
2025-01-23 23:04:29 -0500

    tst

--- hosts/clover/default.nix
+++ hosts/clover/default.nix
@@ -209,14 +209,15 @@
   fonts.fontDir.enable = true;
   fonts.fontconfig.enable = true;
 
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
+  #xdg.portal = {
+  #  enable = true;
+  #  wlr.enable = true;
+  #  extraPortals = [
+  #    pkgs.xdg-desktop-portal-hyprland
+  #    pkgs.xdg-desktop-portal-gtk
+  #  ];
+  #};
+  xdg.portal.config.common.default = "*";
 
 
   # Open ports in the firewall.
