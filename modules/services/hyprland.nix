247089e39147ffae1afcf243a39c5798be020c75
Gui <gfulco4@gmail.com>
2025-01-24 01:51:33 -0500

    tst

--- modules/services/hyprland.nix
+++ modules/services/hyprland.nix
@@ -1,21 +1,15 @@
-{ config, pkgs, lib, ... }: let
+{ config, pkgs, lib, ... }: 
+let
   cfg = config.clover.services.hyprland;
-
 in {
   options = {
-    clover.services.hyprland = { 
-      enable = lib.mkEnableOption "hypr services"; 
-      type = lib.mkOption {
-        description = "Enable Hyprland services";
-      };
-      default = false;
-    }; 
+    clover.services.hyprland.enable = lib.mkEnableOption "hypr services";
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.clover.services.hyprland = {
-    serviceConfig.ExecStart = "${pkgs.hyprland}/bin/hyprland";
-    wantedBy = [ "default.target" ];
+    systemd.user.services.hyprland = {
+      serviceConfig.ExecStart = "${pkgs.hyprland}/bin/hyprland";
+      wantedBy = [ "default.target" ];
     };
   };
 }
