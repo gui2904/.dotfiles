390a9e3c8ea64a39759d05c69608ca22bdeee6ea
Gui <gfulco4@gmail.com>
2025-01-24 05:12:54 -0500

    tst

--- modules/programs/shells/zsh.nix
+++ modules/programs/shells/zsh.nix
@@ -49,10 +49,6 @@ in {
           git branch 2>/dev/null | sed -n '/\*/s/\* \(.*\)/ (\1)/p'
         }
         
-        #export HYPRCURSOR_THEME="Bibata-Modern-Classic"
-        #export HYPRCURSOR_SIZE=24
-        #export XCURSOR_SIZE=24
-        #export XCURSOR_THEME=Bibata-Modern-Classic
         export XDG_SESSION_TYPE=wayland
         export WAYLAND_DISPLAY=wayland-0
         export XDG_RUNTIME_DIR=/run/user/1001
