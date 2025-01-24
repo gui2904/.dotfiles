b7ffb801b4d1e4e8daeef37b4a7f6212b7e8f3ae
Gui <gfulco4@gmail.com>
2025-01-23 23:26:07 -0500

    tst

--- hosts/clover/default.nix
+++ hosts/clover/default.nix
@@ -150,8 +150,8 @@
   
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    #displayManager.gdm.enable = true;
+    #desktopManager.gnome.enable = true;
   };
 
   #programs.hyprland = {
