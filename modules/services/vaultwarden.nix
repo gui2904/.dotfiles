e33889b52e676416063b809bfe53079a746403d6
gui2904 <gfulco4@gmail.com>
2025-01-31 07:47:01 -0500

    test

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -7,17 +7,18 @@
   cfg = config.clover.services.vaultwarden;
 in {
   options.clover.services.vaultwarden = {
-    enable = lib.mkEnableOption "enable zsh";
+    enable = lib.mkEnableOption "Enable Vaultwarden service";
   };
 
   config = lib.mkIf cfg.enable {
     services.vaultwarden = {
       enable = true;
       dbBackend = "postgresql";
-      environmentFile = "/home/server/.vaultwarden/.env";
+      environmentFile = "/home/server/.vaultwarden/.env";  # Full path without '~'
       config = {
         SIGNUPS_ALLOWED = false;
       };
     };
   };
 }
+
