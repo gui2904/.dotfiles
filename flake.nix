821782a7f6a1d7dae71a0b6d30040050277de7c3
Gui <gfulco4@gmail.com>
2025-02-15 16:15:59 -0500

    test

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
   
   nixConfig = {
     extra-substituters = [
+      "https://hyprland.cachix.org"
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       # "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
@@ -59,6 +60,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # hyprland.url = "github:hyprwm/Hyprland";
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
