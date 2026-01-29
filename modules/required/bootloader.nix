42af6502cf75d5931560ff6d5b678930667e1f82
gui2904 <gfulco4@gmail.com>
2026-01-29 07:49:22 -0500

    Fix

--- modules/required/bootloader.nix
+++ modules/required/bootloader.nix
@@ -1,15 +1,18 @@
-{...}: {
-  #boot.loader.grub = {
-  #  enable = true;
-  #  efiSupport = true;
-  #  device = "nodev"; # do NOT use /dev/nvme0n1 in EFI mode
-  #};
+{ config, lib, ... }:
+{
+  config = lib.mkMerge [
+    (lib.mkIf (config.networking.hostName == "clover") {
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.grub.enable = false;
+    })
 
-  #boot.loader.efi = {
-  #  canTouchEfiVariables = true;
-  #};
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = false;
+    (lib.mkIf (config.networking.hostName == "rasp") {
+      boot.loader.systemd-boot.enable = false;
+      boot.loader.efi.canTouchEfiVariables = false;
+      boot.loader.grub.enable = false;
+      boot.loader.generic-extlinux-compatible.enable = true;
+    })
+  ];
 }
+
