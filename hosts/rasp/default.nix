204f84642101a8d1a63b4e4f8055ba6f93b7c907
gui2904 <gfulco4@gmail.com>
2026-02-05 02:30:14 -0500

    Fix

--- hosts/rasp/default.nix
+++ hosts/rasp/default.nix
@@ -79,20 +79,20 @@
     };
   };
 
- # services.forgejo = {
- #   enable = true;
- #   stateDir = "/var/lib/forgejo";
+  services.forgejo = {
+    enable = true;
+    stateDir = "/var/lib/forgejo";
 
- #   settings = {
- #     server = {
- #       DOMAIN = "localhost";
- #       HTTP_PORT = 3000;
- #       ROOT_URL = "http://localhost:3000/";
- #     };
+    settings = {
+      server = {
+        DOMAIN = "localhost";
+        HTTP_PORT = 3000;
+        ROOT_URL = "http://localhost:3000/";
+      };
 
- #     service.DISABLE_REGISTRATION = false;
- #   };
- # };
+      service.DISABLE_REGISTRATION = false;
+    };
+  };
 
   services.vaultwarden = {
     enable = true;
@@ -100,14 +100,13 @@
     config = {
       ROCKET_ADDRESS = "0.0.0.0";
       ROCKET_PORT = 8222;
-      SIGNUPS_ALLOWED = false; # safer for personal server
     };
   };
  
 
   # Open ports in the firewall.
   #networking.firewall.trustedInterfaces = [ "wlan0" ]; # replace with your LAN interface
-  networking.firewall.allowedTCPPorts = [ 22 8222 ]; #SSH and Vaultwarden
+  networking.firewall.allowedTCPPorts = [ 22 8222 3000 ]; #SSH and Vaultwarden
 
 
   system.stateVersion = "24.05"; # Did you read the comment?
